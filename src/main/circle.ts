aac946b986f95f025ab78bd4346f37fb13d5029c
azurepolarbear <azurepolarbear@users.noreply.github.com>
2025-03-01 20:17:55 -0600

    Add initial classes for circle splatter.

---
+++ src/main/circle.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2025 brittni and the polar bear LLC.
+ *
+ * This file is a part of azurepolarbear's splatter algorithmic art project,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * The visual outputs of this source code are licensed under the
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0) License.
+ * You should have received a copy of the CC BY-NC-ND 4.0 License with this program.
+ * See OUTPUT-LICENSE or go to https://creativecommons.org/licenses/by-nc-nd/4.0/
+ * for full license details.
+ */
+
+import { CanvasRedrawListener, Coordinate } from "@batpb/genart";
+
+export class Circle implements CanvasRedrawListener {
+    #cornerCoordinateA: Coordinate;
+    #cornerCoordinateB: Coordinate;
+
+    #children: Circle[];
+
+    public constructor() {
+        this.#cornerCoordinateA = new  Coordinate();
+        this.#cornerCoordinateB = new Coordinate();
+        this.#children = [];
+    }
+
+    public canvasRedraw(): void {
+        this.#cornerCoordinateA.remap();
+        this.#cornerCoordinateB.remap()
+        this.#children.forEach((child: Circle): void => {
+            child.canvasRedraw();
+        });
+    }
+
+    public draw(): void {
+    }
+}
